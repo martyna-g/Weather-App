a336af64482913d868c20a7ff69294c4e64f4c95
Martyna Gorna <mart.gorna@gmail.com>
2020-09-05 21:09:19 +0100

    Dispatch fetchForecastSuccess from thunk

--- app/src/actions/fetchWeather.js
+++ app/src/actions/fetchWeather.js
@@ -1,20 +1,35 @@
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
+export const FETCH_FORECAST = "FETCH_FORECAST";
 
 export const fetchWeatherSuccess = (payload) => ({
   type: FETCH_WEATHER,
-  payload,
+  payload
 });
 
-export const fetchWeatherAPI = (lat, lon) => {
-  const targetUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  return async dispath => {
-    const response = await fetch(targetUrl);
+export const fetchForecastSuccess = (payload) => ({
+  type: FETCH_FORECAST,
+  payload
+});
+
+export const fetchWeatherAPI = (city) => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}`;
+
+  const getAPI = async (url) => {
+    const response = await fetch(url);
     if (response.status === 200) {
       const data = await response.json();
-      dispath(fetchWeatherSuccess(data));
+      return data;
     }
   }
+  return async dispath => {
+    const weatherData = await getAPI(weatherUrl);
+    dispath(fetchWeatherSuccess(weatherData));
+    
+    const forecastData = await getAPI(forecastUrl);
+    dispath(fetchForecastSuccess(forecastData));
+    }
 }
 
