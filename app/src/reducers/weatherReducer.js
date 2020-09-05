17ffc2b94373dc00949ab1c4655d031aee7ec11e
Martyna Gorna <mart.gorna@gmail.com>
2020-09-05 13:43:05 +0100

    Update weather reducer and initial state

--- app/src/reducers/weatherReducer.js
+++ app/src/reducers/weatherReducer.js
@@ -1,29 +1,37 @@
 import { FETCH_WEATHER } from "../actions/fetchWeather";
 
 const initialState = {
-  current: {
-    temp: 0,
+  city: {
+    name: "",
     dt: 0,
-    humidity: 0
+  },
+  sys: {
+    country: "",
+  },
+  main: {
+    temp: 0,
+    humidity: 0,
   },
   weather: {
-    description: '',
     id: 0,
-    main: ''
-  }
+    main: "",
+    description: "",
+  },
 };
 
 const weatherReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_WEATHER:
-        return {
-          ...state,
-          current: action.payload.current,
-          weather: action.payload.current.weather[0]
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default weatherReducer;
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return {
+        ...state,
+        city: action.payload,
+        sys: action.payload.sys,
+        weather: action.payload.weather[0],
+        main: action.payload.main,
+      };
+    default:
+      return state;
+  }
+};
+
+export default weatherReducer;
