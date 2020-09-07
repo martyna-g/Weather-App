a0c943e2f9e727c66db296eabcd78a640bec3a41
Martyna Gorna <mart.gorna@gmail.com>
2020-09-07 12:34:59 +0100

    Updates

---
+++ precache-manifest.365be9b011d445e892292d08fbbff595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c7f854581ed69ff38b1d1ee51c42bf",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "0fe504f3ba0d0b88f386",
+    "url": "/Weather-App/static/css/main.5c9fb9f8.chunk.css"
+  },
+  {
+    "revision": "4ce3896f58f441e135c7",
+    "url": "/Weather-App/static/js/2.5c9f9c96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-App/static/js/2.5c9f9c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe504f3ba0d0b88f386",
+    "url": "/Weather-App/static/js/main.56958e8f.chunk.js"
+  },
+  {
+    "revision": "5544dd564f6652b0aab4",
+    "url": "/Weather-App/static/js/runtime-main.0f68437e.js"
+  },
+  {
+    "revision": "99840be35d83597e2dbab59e6a09c496",
+    "url": "/Weather-App/static/media/bckgrd.99840be3.jpg"
+  }
+]);
