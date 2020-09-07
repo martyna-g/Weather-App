bae423792914a0e3a4cf4bf5806426d4963c5065
Martyna Gorna <mart.gorna@gmail.com>
2020-09-07 09:33:19 +0100

    Update API url

--- app/src/actions/fetchWeather.js
+++ app/src/actions/fetchWeather.js
@@ -15,7 +15,7 @@ export const fetchForecastSuccess = (payload) => ({
 
 export const fetchWeatherAPI = (city) => {
   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}&units=metric`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`;
 
   const getAPI = async (url) => {
     const response = await fetch(url);
